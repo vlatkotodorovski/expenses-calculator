22620a740232feed4cca6f2dd59aa545780bdad4
Stefan <stefangorgevik@hotmail.com>
2019-05-09 21:45:32 +0200

    initial commit react

---
+++ React/expenses-calculator/src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+import {Login} from '../Login/Login'
+import {Register} from '../Register/Register'
+import './App.css'
+
+export class App extends React.Component {
+    render() {
+        return(
+            <main id="app">
+                <Route exact path = "/" component = {Login} />
+                <Route path = "/register" component = {Register} />
+            </main>
+
+        )
+    }
+}
