2d47d5743485e13293f0f5340fc45d80d735d274
vlatkotodorovski <vlatko_ku@hotmail.com>
2019-05-10 21:46:07 +0200

    created new components Register , Portal , Expenses , Products

--- react/expenses-calculator/src/components/App/App.js
+++ react/expenses-calculator/src/components/App/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 import {Login} from '../Login/Login'
 import {Register} from '../Register/Register'
-import {NewProduct} from '../NewProduct/NewProduct'
+import {Portal} from '../Portal/Portal'
+// import {NewProduct} from '../NewProduct/NewProduct'
+// import {Products} from '../Products/Products'
+// import {Expenses} from '../Expenses/Expenses'
 import './App.css'
 
 export class App extends React.Component {
@@ -11,7 +14,11 @@ export class App extends React.Component {
             <main id="app">
                 <Route exact path = "/" component = {Login} />
                 <Route path = "/register" component = {Register} />
-                <Route path = "/new-product" component = {NewProduct} />
+                <Portal />
+                {/* <Route path = "/new-product" component = {NewProduct} />
+                <Route path = "/products" component = {Products} />
+                <Route path = "/expenses" component = {Expenses} /> */}
+
             </main>
 
         )
