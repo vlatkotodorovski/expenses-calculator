8553160c932a7a6e11d5fd2888e61e3d7f27d3fa
vlatkotodorovski <vlatko_ku@hotmail.com>
2019-05-10 17:43:26 +0200

    added NewProduct component without css

--- react/expenses-calculator/src/components/App/App.js
+++ react/expenses-calculator/src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 import {Login} from '../Login/Login'
 import {Register} from '../Register/Register'
+import {NewProduct} from '../NewProduct/NewProduct'
 import './App.css'
 
 export class App extends React.Component {
@@ -10,6 +11,7 @@ export class App extends React.Component {
             <main id="app">
                 <Route exact path = "/" component = {Login} />
                 <Route path = "/register" component = {Register} />
+                <Route path = "/new-product" component = {NewProduct} />
             </main>
 
         )
