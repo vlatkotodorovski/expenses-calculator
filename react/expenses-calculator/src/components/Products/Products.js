c8dc83e1b54f7ead9d0100e20a0e47089ce39733
vlatkotodorovski <vlatko_ku@hotmail.com>
2019-05-14 11:44:33 +0200

    added new component Header

--- react/expenses-calculator/src/components/Products/Products.js
+++ react/expenses-calculator/src/components/Products/Products.js
@@ -7,7 +7,7 @@ export class Products extends React.Component {
     render() {
         return (
             <section id="products">
-                <div className="headerNewProduct">
+                {/* <div className="headerNewProduct">
         <div className="button">
             <button className="button_products">PRODUCTS</button>
             <button className="button_expenses">EXPENSES</button> 
@@ -17,7 +17,7 @@ export class Products extends React.Component {
                 alt="avatar"/>
             <p>Gal Gadot</p>
         </div>
-    </div>
+    </div> */}
     <div className="prodText_dropdown">
         <div>
             <h1>Products</h1>
